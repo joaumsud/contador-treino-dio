3f139bacd13745a410782486b1116bc9700f44ad
joaumsud <joaumsud@hotmail.com>
2022-01-14 23:50:35 -0300

    Adicionando ação de adicionar ou subtrair a página.

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+var count = 0;
+
+const number_current = document.getElementById('number');
+
+function increment(){
+    count++;
+    number_current.innerHTML = count;
+}
+
+function decrement(){
+    count--;
+    number_current.innerHTML = count;
+}
