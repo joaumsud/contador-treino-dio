27b023f1734f6357fad405f80edb2488afbd0303
joaumsud <joaumsud@hotmail.com>
2022-01-17 19:47:13 -0300

    Modificando o estilo do contador.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,36 @@
 let count = 0;
+const counter_number = document.getElementById("number");
 
-const number_current = document.getElementById('number');
-
-function increment(){
-    count++;
-    number_current.innerHTML = count;
+function increment() {
+    if (count >= 10) {
+        count += 10;
+        counter_number.innerHTML = count;
+    } else {
+        count++;
+        counter_number.innerHTML = count;
+        counter_number.style.color = "white";
+        if(count < 0){
+            counter_number.style.color = "red"; 
+        }
+    }
 }
 
-function decrement(){
-    count--;
-    number_current.innerHTML = count;
+function decrement() {
+    if (count <= (-10)) {
+        count -= 10;
+        counter_number.innerHTML = count;
+
+    } else {
+        count--;
+        counter_number.innerHTML = count;
+        if (count < 0) {
+            counter_number.style.color = "red";
+        }
+    }
+
+}
+function zerar() {
+    count = 0;
+    counter_number.innerHTML = count;
+    counter_number.style.color = "white";
 }
